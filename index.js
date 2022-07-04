ed7ad0e02830b470977b827bd0f330bf6476ce98
MelonFruit7 <leogofman23@gmail.com>
2022-07-04 15:00:28 -0400

    stuff

---
+++ index.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const client = new Discord.Client({
+    intents:["GUILDS", "GUILD_MESSAGES"]
+});
+const {prefix, token} = require('./config.json');
+
+client.on('messageCreate', (message) => {
+    if (message.content === "ping") {
+        message.channel.send("pong");
+    }
+})
+
+client.on('messageCreate', message => {
+    if (message.content.startsWith(`${prefix}say`) && !message.author.bot) {
+        message.delete();
+        if (message.content.length > 5) message.channel.send(message.content.substring(5,message.content.length));
+    }
+})
+
+client.login(token);
